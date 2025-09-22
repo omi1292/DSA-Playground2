1f1c4021b579316338508390760e650143e6c1eb
omkareshwari pawar <131356752+omi1292@users.noreply.github.com>
2025-09-22 20:47:20 +0530

    Create Kadane.java

---
+++ src/arrays/src/arrays/Kadane.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Kadane {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0], currentSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println("Max Subarray Sum: " + maxSubArray(nums));
+    }
+}
